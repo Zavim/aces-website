898207fea4af908104d8191ab6ffd64bfa2726ee
Zavim <Zark471@gmail.com>
2023-09-20 10:35:46 -0400

    -f start converting home pg to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    mdx({
+      syntaxHighlight: "shiki",
+    }),
+  ],
 });
