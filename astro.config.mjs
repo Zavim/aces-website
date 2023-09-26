db25f8d2189dbdcde045fd143c6f58a926a36218
Zavim <Zark471@gmail.com>
2023-09-26 12:26:01 -0400

    -f add blueBox as storyblok component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,38 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { loadEnv } from "vite";
 
-// https://astro.build/config
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 export default defineConfig({
   integrations: [
     react(),
     mdx({
       syntaxHighlight: "shiki",
     }),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
+      apiOptions: {
+        region: "us",
+      },
+      components: {
+        page: "storyblok/Page",
+        blueBox: "storyblok/BlueBox",
+      },
+      componentsDir: "src",
+      enableFallbackComponent: false,
+      customFallbackComponent: "",
+      useCustomApi: false,
+    }),
   ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
